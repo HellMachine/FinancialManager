1259f591422362a3398cef9d13181b9d6055df78
HellMachine <lxmikh@gmail.com>
2014-08-05 05:59:34 +0400

    Added 2 fields - login and pass, and 2 variants connect'a

--- src/mikh/alexey/finman/logic/DbHelper.java
+++ src/mikh/alexey/finman/logic/DbHelper.java
@@ -10,13 +10,27 @@ import java.sql.SQLException;
 
 public class DbHelper {
 
+    private static Connection con;
+    //TODO перенести поля настройки в отдельный фаил Settings.properties
     private String driver = "org.sqlite.JDBC";
     private String url = "jdbc:sqlite:finman.db";
-    private Connection con;
+    //На случай, если для подключения к базе нужен логин:пасс
+    private String login = "";
+    private String password = "";
 
-    public Connection getConnection() throws ClassNotFoundException, SQLException {
-        Class.forName(driver);
-        con = DriverManager.getConnection(url);
+    public Connection getConnection(){
+        try{
+            Class.forName(driver);
+            if(login.equals("")){
+                con = DriverManager.getConnection(url);
+            }else{
+                con = DriverManager.getConnection(url, login, password);
+            }
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }catch (SQLException e){
+            e.printStackTrace();
+        }
         return con;
     }
 }
