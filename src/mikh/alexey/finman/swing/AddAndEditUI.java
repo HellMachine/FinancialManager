0467e0ecedb02b1dd5313f0090767b9792202c3d
HellMachine <lxmikh@gmail.com>
2014-08-09 03:48:57 +0400

    Шаблон для добавления записей к акку

---
+++ src/mikh/alexey/finman/swing/AddAndEditUI.java
@@ -0,0 +1,18 @@
+package mikh.alexey.finman.swing;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author lxmikh@gmail.com
+ */
+
+public class AddAndEditUI extends JFrame implements ActionListener {
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
