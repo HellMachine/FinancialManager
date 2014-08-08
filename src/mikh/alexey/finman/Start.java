f70a8de898bfc6e05f09e79126eec3b71656e86b
Maaco <lxmikh@gmail.com>
2014-08-08 18:11:31 +0400

    Инициализация LoginUI

--- src/mikh/alexey/finman/Start.java
+++ src/mikh/alexey/finman/Start.java
@@ -1,10 +1,9 @@
 package mikh.alexey.finman;
 
-import mikh.alexey.finman.logic.DbHelper;
+import mikh.alexey.finman.helpers.JFrameHelper;
+import mikh.alexey.finman.helpers.DbHelper;
 import mikh.alexey.finman.swing.LoginUI;
-
 import javax.swing.*;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 /**
@@ -13,18 +12,22 @@ import java.sql.SQLException;
 
 public class Start {
 
+    public static JFrameHelper jfHelper = JFrameHelper.getInstance();
+    public static DbHelper dbHelper = DbHelper.getInstance();
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 
-        SwingUtilities.invokeLater(new Runnable() {
+         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
-
+                createAndShowLoginUI();
             }
         });
     }
 
     private static void createAndShowLoginUI(){
         JFrame loginFrame = new LoginUI();
+        jfHelper.centerFrame(loginFrame);
         loginFrame.setVisible(true);
     }
 
