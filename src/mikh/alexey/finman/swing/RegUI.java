779f35983d25df835ac8d7054e70aa0cdf462bb3
HellMachine <lxmikh@gmail.com>
2014-09-08 05:12:15 +0400

    Finished registration form and reformat code

--- src/mikh/alexey/finman/swing/RegUI.java
+++ src/mikh/alexey/finman/swing/RegUI.java
@@ -11,7 +11,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.geom.Arc2D;
 import java.text.NumberFormat;
 
 /**
@@ -23,28 +22,17 @@ public class RegUI extends JFrame implements ActionListener {
     private static Logger logger = LoggerFactory.getLogger(RegUI.class);
 
     private String alertMessage = "Fill all fields!";
-    private JLabel loginLabel = new JLabel("Login:");
-    private JLabel passLabel = new JLabel("Password:");
-    private JLabel passConfirmLabel = new JLabel("Confirm password:");
-    private JLabel accountFirstName = new JLabel("First account name:");
-    private JLabel accountBalance = new JLabel("Account balance:");
-    private JLabel accountDescLabel = new JLabel("Description:");
     private JLabel alertMessageLabel = new JLabel(alertMessage);
     private JTextField loginField = new JTextField(10);
     private JPasswordField passField = new JPasswordField(10);
     private JPasswordField passConfirmField = new JPasswordField(10);
     private JTextField accountNameField = new JTextField(10);
     private JTextArea accountDescripton = new JTextArea(5, 5);
-    private JButton createUserButton = new JButton("Create user");
-    private JScrollPane descPane = new JScrollPane(accountDescripton);
     private JFormattedTextField balanceField;
-    private NumberFormat balanceFormat;
     private JComboBox avatarList;
-    private JPanel regPanel = new JPanel();
 
     private LogicSystem logicSystem;
-    public static String[] avatarFilesList = new String[]{
-            "angry.png", "clown.png", "emo.png", "ninja.png", "santa.png", "sweating.png"};
+    public  String[] avatarFilesList;
 
     public RegUI(LogicSystem logicSystem) {
         super("New user");
@@ -52,6 +40,7 @@ public class RegUI extends JFrame implements ActionListener {
         setResizable(false);
 
         this.logicSystem = logicSystem;
+        avatarFilesList = Util.getInstance().fillFileList();
 
         DefaultComboBoxModel<String> modelComboBox = new DefaultComboBoxModel<>();
         for (String avatarFileName : avatarFilesList) {
@@ -69,15 +58,18 @@ public class RegUI extends JFrame implements ActionListener {
             }
         });
 
-        balanceFormat = NumberFormat.getNumberInstance();
+        NumberFormat balanceFormat = NumberFormat.getNumberInstance();
         balanceField = new JFormattedTextField(balanceFormat);
 
+        JScrollPane descPane = new JScrollPane(accountDescripton);
         descPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
         descPane.setAlignmentX(LEFT_ALIGNMENT);
         accountDescripton.setLineWrap(true);
 
+        JButton createUserButton = new JButton("Create user");
         createUserButton.addActionListener(this);
 
+        JPanel regPanel = new JPanel();
         regPanel.setLayout(new GridBagLayout());
 
         regPanel.add(alertMessageLabel, new GridBagConstraints(0, 0, 2, 1, 1, 1,
@@ -89,18 +81,21 @@ public class RegUI extends JFrame implements ActionListener {
         regPanel.add(avatarList, new GridBagConstraints(1, 1, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
+        JLabel loginLabel = new JLabel("Login:");
         regPanel.add(loginLabel, new GridBagConstraints(0, 2, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
         regPanel.add(loginField, new GridBagConstraints(1, 2, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
+        JLabel passLabel = new JLabel("Password:");
         regPanel.add(passLabel, new GridBagConstraints(0, 3, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
         regPanel.add(passField, new GridBagConstraints(1, 3, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
+        JLabel passConfirmLabel = new JLabel("Confirm password:");
         regPanel.add(passConfirmLabel, new GridBagConstraints(0, 4, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
@@ -110,18 +105,21 @@ public class RegUI extends JFrame implements ActionListener {
         regPanel.add(new JSeparator(), new GridBagConstraints(0, 5, 2, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
+        JLabel accountFirstName = new JLabel("First account name:");
         regPanel.add(accountFirstName, new GridBagConstraints(0, 6, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
         regPanel.add(accountNameField, new GridBagConstraints(1, 6, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
+        JLabel accountBalance = new JLabel("Account balance:");
         regPanel.add(accountBalance, new GridBagConstraints(0, 7, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
         regPanel.add(balanceField, new GridBagConstraints(1, 7, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
+        JLabel accountDescLabel = new JLabel("Description:");
         regPanel.add(accountDescLabel, new GridBagConstraints(0, 8, 1, 1, 1, 1,
                 GridBagConstraints.NORTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
@@ -134,30 +132,32 @@ public class RegUI extends JFrame implements ActionListener {
 
         setContentPane(regPanel);
         pack();
-
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         User newUser;
         Account newAccount;
+
         String avatarName = avatarList.getSelectedItem().toString();
         String login = loginField.getText();
         String pass = Util.getInstance().getHash(new String(passField.getPassword()));
         String passConfirm = Util.getInstance().getHash(new String(passConfirmField.getPassword()));
-        String accountName = accountNameField.getText();
-        Double balance = ((Number)balanceField.getValue()).doubleValue();
-        String accountDesc = accountDescripton.getText();
 
-        if (login.isEmpty() || pass.isEmpty() || passConfirm.isEmpty() ||
-                accountName.isEmpty() || balance.equals("") || accountDesc.isEmpty()){
-            alertMessage.toUpperCase();
+        //FIXME здесь в поле balance потенциальная ошибка на вводимые данные!
+        String accountName = accountNameField.getText();
+        Double balance = new Double(balanceField.getText());
+        String accountDesc = accountDescripton.getText();
+        boolean loginDataReady = login.isEmpty() || pass.isEmpty() || passConfirm.isEmpty();
+        boolean accountDataReady = accountName.isEmpty() || balance.equals("") || accountDesc.isEmpty();
+
+        if (loginDataReady || accountDataReady) {
             alertMessageLabel.setForeground(Color.RED);
             return;
-        } else if (logicSystem.isUserExist(login)){
+        } else if (logicSystem.isUserExist(login)) {
             alertMessage = "User with this name EXIST!";
             return;
-        } else if (pass != passConfirm){
+        } else if (!pass.equals(passConfirm)) {
             alertMessage = "Passwords don't match!";
             return;
         } else {
@@ -173,5 +173,4 @@ public class RegUI extends JFrame implements ActionListener {
         }
         Util.getInstance().reLogin(RegUI.this);
     }
-
 }
